763495db3486bc5c5b3fa674417c811f6d871ace
franhaselden <fran@haselden.co.uk>
2016-12-18 18:12:49 +0000

    Wordpress theme setup

---
+++ wp-content/themes/plastic-propaganda/header.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+
+		<?php wp_head(); ?>
+		<script>
+        // conditionizr.com
+        // configure environment tests
+        conditionizr.config({
+            assets: '<?php echo get_template_directory_uri(); ?>',
+            tests: {}
+        });
+        </script>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
+
+	</head>
+	<body <?php body_class(); ?>>
+		<div class="wrapper">
+			<header class="header clear" role="banner">
+					<div class="logo">
+						<a href="<?php echo home_url(); ?>">
+							Plastic Propaganda
+						</a>
+					</div>
+					<nav class="nav" role="navigation">
+						<?php html5blank_nav(); ?>
+					</nav>
+
+			</header>
